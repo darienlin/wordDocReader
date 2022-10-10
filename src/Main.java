d2b3f45ddba261c1d2d539d7b8ef0e7043afa33e
darienlin <darienlin87@gmail.com>
2022-10-09 22:19:20 -0700

    doesnt read from "ASIN: " anymore. also gave user options to choose whether they want same id or different id between two files.

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,9 @@ public class Main {
             Scanner kb = new Scanner(System.in);
             String mainPathName;
             String compPathName = "ploop";
-
+            String sameOrDiff;
+            boolean same = false;
+            boolean diff = false;
 
             System.out.print("Enter the main file (bigger file, ex: \"D:\\MBZ10.04\\022822 contiuned sell\\091622_Jackpot Wireless eBay_CONTINUE SELL.docx\"): ");
             mainPathName = kb.nextLine();
@@ -26,6 +28,16 @@ public class Main {
             System.out.print("Enter the comparing file (smaller file, ex: \"D:\\MBZ10.04\\022822 contiuned sell\\Ebay_Jackpot Wireless_03012022.docx\"): ");
             compPathName = kb.nextLine();
             compPathName = compPathName.substring(1, compPathName.length() - 1);
+
+            System.out.print("Do you want the same or different ASIN in files (same, diff): ");
+            sameOrDiff = kb.nextLine();
+            System.out.print("\n");
+
+            if(sameOrDiff.equals("same"))
+                same = true;
+
+            if(sameOrDiff.equals("diff"))
+                diff = true;
 
             //runs until user types stop
             while (!compPathName.equals("stop")) {
@@ -48,43 +60,68 @@ public class Main {
 
                 //final ASIN list that contains asin/ebay ID that is present in comp but not in main
                 ArrayList<String> finalAsinList = new ArrayList<String>();
-                boolean diff = false;
+                boolean diff1 = false;
                 boolean equAsin = false;
 
 
                //loops through comp file
                 for (XWPFParagraph compParagraph : compParagraphList) {
                     String compAsin = compParagraph.getText();
+                    String mainAsin = "";
+
+                    if(compAsin.length() < 6)
+                        continue;
+
                     //if doesnt have id or asin reads html
+                    if(compAsin.indexOf("ebay") >= 0)
                     if (compAsin.indexOf("https://") >= 0){
                         String []compAsinHttpParts = (compAsin.split("/"));
                         compAsin = compAsinHttpParts[4].substring(0,12);
                         //System.out.println("compAsin " + compAsin);
                     }
 
-                    //finds asin if cant find then reads for ebay or asin without "ASIN: "
+                    if(compAsin.indexOf("amazon") >= 0)
+                        if (compAsin.indexOf("https://") >= 0){
+                            String []compAsinHttpParts = (compAsin.split("/"));
+                            compAsin = compAsinHttpParts[5].substring(0,10);
+                            //System.out.println("compAsin " + compAsin);
+                        }
+
+                    //finds ebay or asin"
                     if (compAsin.length() > 4) {
-                        if (compAsin.substring(0, 4).equals("ASIN"))
-                            compAsin = compAsin.substring(8, 18);
-                        else if (compAsin.length() == 10 || compAsin.length() == 11)
+                        if (compAsin.length() == 10 || compAsin.length() == 11)
                             compAsin = compAsin.substring(0, 10);
                         else if (compAsin.length() == 12)
                             compAsin = compAsin.substring(0, 12);
                         else continue;
                     }
+
+                    //System.out.println(compAsin);
                     //loops through main to compare
                     for (XWPFParagraph mainParagraph : mainParagraphList) {
-                        String mainAsin = mainParagraph.getText();
+                        mainAsin = mainParagraph.getText();
+
+                        if(mainAsin.length() < 6)
+                            continue;
+
                         //same as above
+                        if(mainAsin.indexOf("ebay") >= 0)
                         if (mainAsin.indexOf("https://") >= 0){
                             String []mainAsinHttpParts = (mainAsin.split("/"));
                             mainAsin = mainAsinHttpParts[4].substring(0,12);
+                            //System.out.println("mainAsin " + mainAsin);
                         }
+
+                        if(mainAsin.indexOf("amazon") >= 0)
+                            if (mainAsin.indexOf("https://") >= 0){
+                                String []mainAsinHttpParts = (mainAsin.split("/"));
+                                mainAsin = mainAsinHttpParts[5].substring(0,10);
+                                //System.out.println("mainAsin " + mainAsin);
+                            }
+
                         //same as above
                         if (mainAsin.length() > 4) {
-                            if (mainAsin.substring(0, 4).equals("ASIN"))
-                                mainAsin = mainAsin.substring(8, 18);
-                            else if (mainAsin.length() == 10 || mainAsin.length() == 11)
+                            if (mainAsin.length() == 10 || mainAsin.length() == 11)
                                 mainAsin = mainAsin.substring(0, 10);
                             else if (mainAsin.length() == 12)
                                 mainAsin = mainAsin.substring(0, 12);
@@ -92,16 +129,31 @@ public class Main {
                         }
 
                         //if comp is main then breaks loop but if it isn't then is added
-                        if (compAsin.equals(mainAsin)) {
-                            diff = false;
-                            break;
-                        } else {
-                            diff = true;
+                        if(diff) {
+                            System.out.println("compAsin " + compAsin + " mainAsin " + mainAsin + " boolean " + compAsin.equals(mainAsin));
+                            if (compAsin.equals(mainAsin)) {
+                                diff1 = false;
+                                break;
+                            } else {
+                                diff1 = true;
+                            }
+                        }
+
+                        if(same) {
+                            //System.out.println("compAsin " + compAsin + " mainAsin " + mainAsin + " boolean " + compAsin.equals(mainAsin));
+                            if (compAsin.equals(mainAsin)) {
+                                diff1 = true;
+                                break;
+                            } else {
+                                diff1 = false;
+                            }
                         }
                     }
 
+                    //System.out.println("compAsin " + compAsin + " mainAsin" + mainAsin + " boolean" + mainAsin.equals(compAsin));
+
                     //adds to finalList
-                    if (diff)
+                    if (diff1)
                         compStrParagraphList.add(compAsin);
 
                 }
@@ -111,19 +163,41 @@ public class Main {
                 if(compStrParagraphList.size() == 0)
                     System.out.println("Everything in comparing file is in main file");
 
-                else {
+                else if(diff){
                     System.out.println("ASIN or ebay ID that is in the comparing file but not in the main file: ");
+                    for (int i = 0; i < compStrParagraphList.size(); i++)
+                        System.out.println(compStrParagraphList.get(i));
+                }
+
+                else if(same){
+                    System.out.println("ASIN or ebay ID that is in both comparing and main file: ");
                     for (int i = 0; i < compStrParagraphList.size(); i++)
                         System.out.println(compStrParagraphList.get(i));
                 }
 
                 System.out.println("\n");
 
+
+                //asks whether if the user wants to keep comparing
                 System.out.print("Enter the comparing file (smaller file, ex: \"D:\\MBZ10.04\\022822 contiuned sell\\Ebay_Jackpot Wireless_03012022.docx\") or \"stop\" to stop run: ");
                 compPathName = kb.nextLine();
                 if(compPathName.equals("stop"))
                     break;
                 compPathName = compPathName.substring(1, compPathName.length() - 1);
+
+                //ask if user wants to be diff or same
+                System.out.print("Do you want the same or different ASIN in files (same, diff): ");
+                sameOrDiff = kb.nextLine();
+                System.out.print("\n");
+
+                if(sameOrDiff.equals("same")){
+                    same = true;
+                    diff = false;
+                }
+                if(sameOrDiff.equals("diff")){
+                    same = false;
+                    diff = true;
+                }
             }
 
             } catch(FileNotFoundException e){
